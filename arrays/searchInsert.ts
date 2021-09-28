f19429761745c46e82f406ccfa097f1988ebe5b8
magdabaj <magdabaj@protonmail.com>
2021-09-28 18:06:17 +0200

    searchInsert

---
+++ arrays/searchInsert.ts
@@ -0,0 +1,16 @@
+nums = [1,3,5,6], target = 7
+
+const searchInsert = (nums: number[], target: number): number => {
+    let begin = 0
+    let end = nums.length-1
+    while(begin<=end){
+        let mid = Math.floor(begin+ (end-begin)/2)
+        if (nums[mid]===target) return mid
+        if(nums[mid]>target) end=mid-1
+        else begin = mid + 1
+    }
+    return end+1
+};
+
+
+
